0a1c03e2008904ad6d5ecb3e7f362e5b07f034bc
adria <adrian.saavedra01@uceva.edu.co>
2025-05-22 12:07:01 -0500

    Se ha creado el servicio solicitar servicios completo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import usersRouter from "./routes/auth-users.routes.js";
 import nivelesEducativosRouter from "./routes/niveles_educativos.routes.js"; 
 import clientesRouter from "./routes/clientes.routes.js";
 import prestadoresRouter from "./routes/prestadores_servicios.routes.js"; // Futura implementación
-
+import solicitudesServiciosRoutes from "./routes/solicitudes_servicios.routes.js";
 //import solicitudesRouter from "./routes/solicitudes.routes.js"; // Futura implementación
 
 
@@ -19,6 +19,7 @@ app.use("/api/auth", usersRouter);   //Ruta de autenticación de usuario
 app.use("/api", nivelesEducativosRouter);
 app.use("/api", clientesRouter);    //Ruta de niveles educativos
 // app.use("/api/solicitudes", solicitudesRouter); // Descomentar cuando esté implementado
+app.use("/api", solicitudesServiciosRoutes);
 app.use("/api", prestadoresRouter); // Descomentar cuando esté implementado
 
 // Manejo de errores 404
